f4aa2995b8087253d4b5d17f79ce481be7704422
Adalberto Maia <btowmaia@gmail.com>
2022-03-26 08:09:01 -0300

    26-03-2022

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -122,7 +122,22 @@ class UserController{
           let field = form.querySelector('[name='+name.replace('_', '')+']');
           
           if(field){
-            if(field.type == 'file') continue;
+            switch (field.type) {
+              case 'file':
+                continue;
+                break;
+              case 'radio':
+                field = form.querySelector('[name='+name.replace('_', '')+'][value='+json[name]+']');
+                field.checked = true;
+                break;
+              case 'checkbox':
+                field.checked = json[name];
+                break;
+              default:
+                field.value = json[name];
+                break;
+            }
+            
             field.value = json[name];
           }
           
